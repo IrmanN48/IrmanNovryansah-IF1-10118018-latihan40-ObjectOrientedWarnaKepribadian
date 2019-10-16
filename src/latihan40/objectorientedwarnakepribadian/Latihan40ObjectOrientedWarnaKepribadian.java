ce9b98aede8fcc09fd081f81a10f360e7fe8841f
IRMAN NOVRYANSAH <55683592+IrmanN48@users.noreply.github.com>
2019-10-16 23:10:31 +0700

    Add files via upload

---
+++ src/latihan40/objectorientedwarnakepribadian/Latihan40ObjectOrientedWarnaKepribadian.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihan40.objectorientedwarnakepribadian;
+
+/**
+ *
+ * @author Aero
+ *NAMA     : Irman Novryansah
+ *KELAS    : IF-1
+ *NIM      : 10118018
+ *Deskripsi Program : Program ini untuk menampilkan tampilan dari warna dan 
+ *                     kepribadian.
+ */
+public class Latihan40ObjectOrientedWarnaKepribadian {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        Warna skuyMu = new Warna();
+        
+        skuyMu.ioWarnaMu();
+    }
+    
+}
